a5e21e9968d8bde493adfa7a08d9d4fb1fc0a8ca
karthickelango <karthik5ive@gmail.com>
2024-02-11 13:28:34 +0530

    add footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import image from '../images/profile_pic.jpeg'
 import { TabTitle } from '../utils/GeneralFunctionn'
 
 
+
 const Home = () => {
   TabTitle('Home')
   return (
@@ -10,7 +11,7 @@ const Home = () => {
       <div class="container mt-30">
         <div class="row">
           <div class="col">
-            <h1 className='text-48'>
+            <h1 className='text-48 wow fadeInDown'>
               I’m Karthick Elango. I am frontend developer in React & full stack in MERN.
             </h1>
             <div className='mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400'>
