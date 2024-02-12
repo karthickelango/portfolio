a23655383087201e3e1081dfc39903fe2987f937
karthickelango <karthik5ive@gmail.com>
2024-02-12 20:37:23 +0530

    add model

--- src/component/HireMe.js
+++ src/component/HireMe.js
@@ -11,7 +11,7 @@ const HireMe = () => {
 
     return (
         <>
-            <button onClick={() => setOpen(!open)}>Hire me</button>
+            <button onClick={() => setOpen(!open)}  className='btn bg-success bg-success inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto'>Hire me</button>
             <Transition.Root show={open} as={Fragment}>
                 <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
                     <Transition.Child
