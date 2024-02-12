d71392cd08061cd198df6539aeaa3f25e044f8e2
karthickelango <karthik5ive@gmail.com>
2024-02-12 18:49:24 +0530

    add project

--- src/pages/Qualification.js
+++ src/pages/Qualification.js
@@ -82,6 +82,45 @@ const Qualification = () => {
                         </div>
                     </div>
                 </section>
+                <div className='pb-0'>
+                    <p className='main-title  text-center mb-0'>
+                        <p>Education</p>
+                    </p>
+                </div>
+                <section className='md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40'>
+                    <div className="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
+                        <h2 id=":S1:" className="text-sm font-semibold text-zinc-800 dark:text-zinc-100">Karpagam college of engineering</h2>
+                        <div className="md:col-span-3">
+                            <div className="space-y-16">
+                                <article className="group relative flex flex-col items-start">
+                                    <h3 className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                                        <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50"></div>
+                                        <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
+                                        <span className="relative z-10 role-title">Information technology(B-Tech)</span>
+                                    </h3>
+                                    <p className="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5"><span className="absolute inset-y-0 left-0 flex items-center" aria-hidden="true"><span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span></span>2009 - 2013 (Coimbatore)</p>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className='md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40'>
+                    <div className="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
+                        <h2 id=":S1:" className="text-sm font-semibold text-zinc-800 dark:text-zinc-100">R.K.R high school</h2>
+                        <div className="md:col-span-3">
+                            <div className="space-y-16">
+                                <article className="group relative flex flex-col items-start">
+                                    <h3 className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                                        <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50"></div>
+                                        <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
+                                        <span className="relative z-10 role-title">Computer science</span>
+                                    </h3>
+                                    <p className="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5"><span className="absolute inset-y-0 left-0 flex items-center" aria-hidden="true"><span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span></span>2007 - 2009 (Tiruppur)</p>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         </>
     )
