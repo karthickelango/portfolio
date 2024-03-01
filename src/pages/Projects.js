56a73a1995cc5ac3762f3125319be02c129bcf50
karthickelango <karthik5ive@gmail.com>
2024-03-01 13:31:40 +0530

    add file

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -10,9 +10,9 @@ const Projects = () => {
         <>
             <div className='container-item'>
                 <h1 className='text-48 mt-30 wow fadeInDown primary-color'>
-                    Things I’ve made trying to put my dent in the universe.
+                    Things I’ve made trying to put my skills in the universe.
                 </h1>
-                <div className="mt-16 sm:mt-20">
+                {/* <div className="mt-16 sm:mt-20">
                     <ul role="list" className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3">
                         <li className="group relative flex flex-col items-start">
                             <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
@@ -20,11 +20,18 @@ const Projects = () => {
                             </div>
                             <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
                                 <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50"></div>
-                                <a href="https://tweetxapp.netlify.app/" target='_blank'><span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span><span className="relative z-10">Tweet app (MERN)</span></a>
+                                <a href="https://tweetxapp.netlify.app/" target='_blank'><span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span><span className="relative z-10 relative z-10 role-title">Tweet app (MERN)</span></a>
                             </h2>
                             <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">A Collaborative Tweeting Experience</p>
                         </li>
                     </ul>
+                </div> */}
+                <div className="row mt-16 sm:mt-20">
+                    <div className='col-6'>Hello</div>
+                    <div className='col-6'>
+                        <div><span className="relative z-10 relative z-10 role-title">Tweet app (MERN)</span></div>
+                        <span className='text-sm text-zinc-600 dark:text-zinc-400'>Project Description</span>
+                    </div>
                 </div>
             </div>
         </>
