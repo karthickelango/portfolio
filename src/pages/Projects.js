9e646df1e8fbbde96b345add78dae55223e16ac5
karthickelango <karthik5ive@gmail.com>
2024-03-08 15:20:53 +0530

    add project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,6 +5,7 @@ import screen2 from '../images/screen-2.png'
 import screen3 from '../images/screen-3.png'
 import screen4 from '../images/screen-4.png'
 import screen5 from '../images/screen-5.png'
+import shopping from '../images/shopping.PNG'
 
 
 const Projects = () => {
@@ -16,11 +17,12 @@ const Projects = () => {
                     Things I've made trying to put my skills out there.
                 </h1>
                 <div className="row mt-16 sm:mt-20">
+                    {/* first col */}
                     <div className='col-md-6 col-sm-12 projec-page'>
                         <div className='content-bg'>
                             <a href='https://tweetxapp.netlify.app/' target='_blank'>
-                                <p className="relative z-10 relative z-10 role-title">Tweet app (MERN)</p>
-                                <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400 role-sub-title">A Collaborative Tweeting Experience</p>
+                                <p className="relative z-10 relative z-10 role-title mb-3">Tweet app (MERN)</p>
+                                {/* <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400 role-sub-title">A Collaborative Tweeting Experience</p> */}
                                 <div className="grid grid-flow-col grid-rows-2 grid-cols-3 gap-8 group-hover:scale-100 group-hover:opacity-100">
                                     <div>
                                         <img src={screen3} alt="" loading="lazy" />
@@ -36,6 +38,20 @@ const Projects = () => {
                                     </div>
                                     <div>
                                         <img src={screen1} alt="" loading="lazy" />
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    {/* second col */}
+                    <div className='col-md-6 col-sm-12 projec-page'>
+                        <div className='content-bg'>
+                            <a href='https://angaddi.netlify.app/' target='_blank'>
+                                <p className="relative z-10 relative z-10 role-title mb-3">Shopping app</p>
+                                {/* <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400 role-sub-title">A shopping Experience</p> */}
+                                <div className="grid grid-flow-col">
+                                    <div>
+                                        <img src={shopping} alt="" loading="lazy" />
                                     </div>
                                 </div>
                             </a>
