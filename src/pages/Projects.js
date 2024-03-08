1da2715449fff081d1bfd3365153c5e464247e3d
karthickelango <karthik5ive@gmail.com>
2024-03-08 16:24:21 +0530

    add tag

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -19,8 +19,8 @@ const Projects = () => {
                 <div className="row mt-16 sm:mt-20">
                     {/* first col */}
                     <div className='col-md-6 col-sm-12 projec-page'>
-                        <div className='content-bg'>
-                            <a href='https://tweetxapp.netlify.app/' target='_blank'>
+                        <a href='https://tweetxapp.netlify.app/' target='_blank'>
+                            <div className='content-bg'>
                                 <p className="relative z-10 relative z-10 role-title mb-3">Tweet app (MERN)</p>
                                 {/* <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400 role-sub-title">A Collaborative Tweeting Experience</p> */}
                                 <div className="grid grid-flow-col grid-rows-2 grid-cols-3 gap-8 group-hover:scale-100 group-hover:opacity-100">
@@ -40,13 +40,13 @@ const Projects = () => {
                                         <img src={screen1} alt="" loading="lazy" />
                                     </div>
                                 </div>
-                            </a>
-                        </div>
+                            </div>
+                        </a>
                     </div>
                     {/* second col */}
                     <div className='col-md-6 col-sm-12 projec-page'>
-                        <div className='content-bg'>
-                            <a href='https://angaddi.netlify.app/' target='_blank'>
+                        <a href='https://angaddi.netlify.app/' target='_blank'>
+                            <div className='content-bg'>
                                 <p className="relative z-10 relative z-10 role-title mb-3">Shopping app</p>
                                 {/* <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400 role-sub-title">A shopping Experience</p> */}
                                 <div className="grid grid-flow-col">
@@ -54,8 +54,8 @@ const Projects = () => {
                                         <img src={shopping} alt="" loading="lazy" />
                                     </div>
                                 </div>
-                            </a>
-                        </div>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
