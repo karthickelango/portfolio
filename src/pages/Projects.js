c8ba3ed7147d8936fd4f3ff31aef542e12cd83e3
karthickelango <karthik5ive@gmail.com>
2024-03-08 19:36:39 +0530

    add project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,6 +6,7 @@ import screen3 from '../images/screen-3.png'
 import screen4 from '../images/screen-4.png'
 import screen5 from '../images/screen-5.png'
 import shopping from '../images/shopping.PNG'
+import dashboard from '../images/dashboard.png'
 
 
 const Projects = () => {
@@ -18,7 +19,7 @@ const Projects = () => {
                 </h1>
                 <div className="row mt-16 sm:mt-20">
                     {/* first col */}
-                    <div className='col-md-6 col-sm-12 projec-page'>
+                    <div className='col-md-6 col-sm-12 projec-page mb-5'>
                         <a href='https://tweetxapp.netlify.app/' target='_blank'>
                             <div className='content-bg'>
                                 <p className="relative z-10 relative z-10 role-title mb-3">Tweet app (MERN)</p>
@@ -44,7 +45,7 @@ const Projects = () => {
                         </a>
                     </div>
                     {/* second col */}
-                    <div className='col-md-6 col-sm-12 projec-page'>
+                    <div className='col-md-6 col-sm-12 projec-page mb-5'>
                         <a href='https://angaddi.netlify.app/' target='_blank'>
                             <div className='content-bg'>
                                 <p className="relative z-10 relative z-10 role-title mb-3">Shopping app</p>
@@ -52,6 +53,20 @@ const Projects = () => {
                                 <div className="grid grid-flow-col">
                                     <div>
                                         <img src={shopping} alt="" loading="lazy" />
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    {/* third col */}
+                    <div className='col-md-6 col-sm-12 projec-page mb-5'>
+                        <a href='https://admindsboard.netlify.app/' target='_blank'>
+                            <div className='content-bg'>
+                                <p className="relative z-10 relative z-10 role-title mb-3">Dashboard</p>
+                                {/* <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400 role-sub-title">A shopping Experience</p> */}
+                                <div className="grid grid-flow-col">
+                                    <div>
+                                        <img src={dashboard} alt="" loading="lazy" />
                                     </div>
                                 </div>
                             </div>
